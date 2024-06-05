c9585c21e2b51c95ada8288976f1d8021afdaae9
mokbhai <mokshitjain18@gmail.com>
2024-06-05 09:57:59 +0530

    basic mail apis done

--- ENV.js
+++ ENV.js
@@ -5,5 +5,14 @@ const PORT = process.env.PORT;
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_SECRET_EXPIRY = "1d";
 const MONGODB_URI = process.env.MONGODB_URI;
+const APP_PASSWORD = process.env.APP_PASSWORD;
+const MAIL_ID = process.env.MAIL_ID;
 
-export { PORT, JWT_SECRET_EXPIRY, JWT_SECRET, MONGODB_URI };
+export {
+  PORT,
+  JWT_SECRET_EXPIRY,
+  JWT_SECRET,
+  MONGODB_URI,
+  MAIL_ID,
+  APP_PASSWORD,
+};
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import UserRoutes from "./UserRoutes.js";
 import EventRoutes from "./EventRoutes.js";
 import FileRoutes from "./FileRoutes.js";
+import MailRoutes from "./MailRoutes.js";
 
 const router = express.Router();
 
 router.use("/user", UserRoutes);
 router.use("/event", EventRoutes);
 router.use("/file", FileRoutes);
+router.use("/mail", MailRoutes);
 
 export default router;
